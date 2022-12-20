33e5ad5d35e9a181390038cd40871f512c5921ee
unknown <getlaurekt@gmail.com>
2022-12-20 13:42:15 +0100

    feat(helpers.ts): added type for generating utility colors
    Type that allows for generating full pallete of tailwind colors for specific prefix of class

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,4 +1,10 @@
+import { ColorsVariants, StandardColors } from "../utils/colors";
+
 type TypeSafeExtract<T, U extends T> = T extends U ? T : never;
 type TypeSafeExclude<T, U extends T> = T extends U ? never : T;
 
-export type { TypeSafeExtract, TypeSafeExclude };
+type GenerateUtilColors<P extends string> = `${Lowercase<P>}-${
+  | StandardColors
+  | ColorsVariants}`;
+
+export type { TypeSafeExtract, TypeSafeExclude, GenerateUtilColors };
