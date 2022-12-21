5682d69fd9fca520ce97f2236e358d255abf22fe
unknown <getlaurekt@gmail.com>
2022-12-21 14:20:13 +0100

    feat: added collection type of transforms classes

---
+++ src/classes/transforms.ts
@@ -0,0 +1,9 @@
+import Rotate from "./transforms/rotate";
+import Scale from "./transforms/scale";
+import Skew from "./transforms/skew";
+import TransformOrigin from "./transforms/transformOrigin";
+import Translate from "./transforms/translate";
+
+type TransformsClass = Rotate | Scale | Skew | TransformOrigin | Translate;
+
+export default TransformsClass;
