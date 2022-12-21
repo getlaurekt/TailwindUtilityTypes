7ebdce32169b810d5979b13f8e6d1c214551f8c5
unknown <getlaurekt@gmail.com>
2022-12-21 14:19:21 +0100

    feat: added collection type of svg classes

---
+++ src/classes/svg.ts
@@ -0,0 +1,7 @@
+import Fill from "./svg/fill";
+import Stroke from "./svg/stroke";
+import StrokeWidth from "./svg/strokeWidth";
+
+type SvgClass = Fill | Stroke | StrokeWidth;
+
+export default SvgClass;
