5f7c3f0aa9cbe5801835f04abe9a8a427b2cfc9b
unknown <getlaurekt@gmail.com>
2022-12-21 14:18:27 +0100

    feat: added collection type of sizing classes

---
+++ src/classes/sizing.ts
@@ -0,0 +1,10 @@
+import Height from "./sizing/height";
+import MaxHeight from "./sizing/maxHeight";
+import MaxWidth from "./sizing/maxWidth";
+import MinHeight from "./sizing/minHeight";
+import MinWidth from "./sizing/minWidth";
+import Width from "./sizing/width";
+
+type SizingClass = Height | MaxHeight | MaxWidth | MinHeight | MinWidth | Width;
+
+export default SizingClass;
