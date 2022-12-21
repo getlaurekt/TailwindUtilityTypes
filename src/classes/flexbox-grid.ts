ec4e25b0786b94e0365fc613afa26c4a089119ce
unknown <getlaurekt@gmail.com>
2022-12-21 14:17:00 +0100

    feat: added collection type of Flexbox and grid classes

---
+++ src/classes/flexbox-grid.ts
@@ -0,0 +1,50 @@
+import AlignContent from "./flexbox-grid/alignContent";
+import AlignItems from "./flexbox-grid/alignItems";
+import AlignSelf from "./flexbox-grid/alignSelf";
+import Flex from "./flexbox-grid/flex";
+import FlexBasis from "./flexbox-grid/flexBasis";
+import FlexDirection from "./flexbox-grid/flexDirection";
+import FlexGrow from "./flexbox-grid/flexGrow";
+import FlexShrink from "./flexbox-grid/flexShrink";
+import FlexWrap from "./flexbox-grid/flexWrap";
+import Gap from "./flexbox-grid/gap";
+import GridAutoColumns from "./flexbox-grid/gridAutoColumns";
+import GridAutoFlow from "./flexbox-grid/gridAutoFlow";
+import GridAutoRows from "./flexbox-grid/gridAutoRows";
+import GridColumnStartEnd from "./flexbox-grid/gridColumnStartEnd";
+import GridTemplateCol from "./flexbox-grid/gridTemplateColumns";
+import GridTemplateRows from "./flexbox-grid/gridTemplateRows";
+import JustifyContent from "./flexbox-grid/justifyContent";
+import JustifyItems from "./flexbox-grid/justifyItems";
+import JustifySelf from "./flexbox-grid/justifySelf";
+import Order from "./flexbox-grid/order";
+import PlaceContent from "./flexbox-grid/placeContent";
+import PlaceItems from "./flexbox-grid/placeItems";
+import PlaceSelf from "./flexbox-grid/placeSelf";
+
+type FlexboxGridClass =
+  | AlignContent
+  | AlignItems
+  | AlignSelf
+  | Flex
+  | FlexBasis
+  | FlexDirection
+  | FlexGrow
+  | FlexShrink
+  | FlexWrap
+  | Gap
+  | GridAutoColumns
+  | GridAutoFlow
+  | GridAutoRows
+  | GridColumnStartEnd
+  | GridTemplateCol
+  | GridTemplateRows
+  | JustifyContent
+  | JustifyItems
+  | JustifySelf
+  | Order
+  | PlaceContent
+  | PlaceItems
+  | PlaceSelf;
+
+export default FlexboxGridClass;
