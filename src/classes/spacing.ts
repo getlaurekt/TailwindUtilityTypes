64a9520e9f931ccd849bee2076502e748d118b7c
unknown <getlaurekt@gmail.com>
2022-12-21 14:18:50 +0100

    feat: added collection type of spacing classes

---
+++ src/classes/spacing.ts
@@ -0,0 +1,7 @@
+import Margin from "./spacing/margin";
+import Padding from "./spacing/padding";
+import SpaceBetween from "./spacing/spaceBetween";
+
+type SpacingClass = Margin | Padding | SpaceBetween;
+
+export default SpacingClass;
