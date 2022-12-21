beb078ef336f65c996c93d816c6cad6de6ecd7d9
unknown <getlaurekt@gmail.com>
2022-12-21 14:18:01 +0100

    feat: added collection type of layout classes

---
+++ src/classes/layout.ts
@@ -0,0 +1,44 @@
+import AspectRatio from "./layout/aspectRatio";
+import BoxDecorationBreak from "./layout/boxDecorationBreak";
+import BoxSizing from "./layout/boxSizing";
+import BreakAfter from "./layout/breakAfter";
+import BreakBefore from "./layout/breakBefore";
+import BreakInside from "./layout/breakInside";
+import Clear from "./layout/clear";
+import Columns from "./layout/columns";
+import Container from "./layout/container";
+import Display from "./layout/display";
+import Floats from "./layout/floats";
+import Isolation from "./layout/isolation";
+import ObjectFit from "./layout/objectFit";
+import ObjectPosition from "./layout/objectPosition";
+import Overflow from "./layout/overflow";
+import OverscrollBehavior from "./layout/overscrollBehavior";
+import Placement from "./layout/placement";
+import Position from "./layout/position";
+import Visibility from "./layout/visibility";
+import ZIndex from "./layout/zIndex";
+
+type LayoutClass =
+  | AspectRatio
+  | BoxDecorationBreak
+  | BoxSizing
+  | BreakAfter
+  | BreakBefore
+  | BreakInside
+  | Clear
+  | Columns
+  | Container
+  | Display
+  | Floats
+  | Isolation
+  | ObjectFit
+  | ObjectPosition
+  | Overflow
+  | OverscrollBehavior
+  | Placement
+  | Position
+  | Visibility
+  | ZIndex;
+
+export default LayoutClass;
