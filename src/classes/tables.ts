12d11ae4fc1cd16572448ccc3d9d3a90a2bee469
unknown <getlaurekt@gmail.com>
2022-12-21 14:19:47 +0100

    feat: added collection type of tables classes

---
+++ src/classes/tables.ts
@@ -0,0 +1,7 @@
+import BorderCollapse from "./tables/borderCollapse";
+import BorderSpacing from "./tables/borderSpacing";
+import TableLayout from "./tables/tableLayout";
+
+type TablesClass = BorderCollapse | BorderSpacing | TableLayout;
+
+export default TablesClass;
